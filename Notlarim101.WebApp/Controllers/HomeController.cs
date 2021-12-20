d898067bfb545a7b073024e0ac5fc0cd6e6f4ed0
ozgeergezer <ozgeergezerrr@gmail.com>
2021-12-20 10:29:07 +0300

    Add project files.

---
+++ Notlarim101.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Notlarim101.BusinessLayer;
+using Notlarim101.Entity;
+
+namespace Notlarim101.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Test test = new Test();
+            ////test.InsertTest();
+            ////test.UpdateTest();
+            ////test.DeleteTest();
+            //test.CommentTest();
+
+            NoteManager nm = new NoteManager();
+            
+            return View(nm.GetAllNotes().OrderByDescending(s=>s.ModifiedOn).ToList());
+        }
+
+        
+        public ActionResult ByCategoryId(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            CategoryManager cm = new CategoryManager();
+            Category cat = cm.GetCategoryById(id.Value);
+
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Index", cat.Notes.OrderByDescending(s => s.ModifiedOn).ToList());
+        }
+        
+        public ActionResult ByCategoryTitle(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            CategoryManager cm = new CategoryManager();
+            Category cat = cm.GetCategoryByTitle(id);
+
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Index", cat.Notes.OrderByDescending(s => s.ModifiedOn).ToList());
+        }
+    }
+}
